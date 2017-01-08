bf5e174dd59d8af5e51a2b29daccd9470ee5f3da
logiclogue <jordanpaullord@hotmail.co.uk>
2017-01-08 21:50:10 +0000

    Adds code for input method, needs fixing

--- src/Board.c
+++ src/Board.c
@@ -17,17 +17,43 @@ Board *Board_new(void)
 
 static int input(Board *self, char column, char colour)
 {
-    return column * colour;
+    int is_column_full = self->state[column][BOARD_ROWS] != " ";
+    int is_valid_column = column >= 0 && column < BOARD_COLUMNS;
+
+    if (is_column_full && is_valid_row) {
+        return 0;
+    }
+
+    int x;
+    char current_square;
+
+    for(y = 0; y < BOARD_ROWS; y++) {
+        current_square = self->state[column][y];
+
+        if (current_square != " ")
+        {
+            continue;
+        }
+
+        self->state[column][y] = colour;
+
+        break;
+    }
+
+    return 1;
 }
 
 static void allocate_state(Board *self)
 {
     self->state = malloc(BOARD_COLUMNS * sizeof(char *));
 
-    int x;
+    int x, y;
 
-    for (x = 0; x < BOARD_COLUMNS; x++)
-    {
+    for (x = 0; x < BOARD_COLUMNS; x++) {
         self->state[x] = malloc(BOARD_ROWS * sizeof(char));
+
+        for (y = 0; y < BOARD_ROWS; y++) {
+            self->state[x][y] = " ";
+        }
     }
 }
