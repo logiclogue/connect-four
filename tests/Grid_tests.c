55f7150bc23cd84acfc1d588b5f958bbeb263961
logiclogue <jordanpaullord@hotmail.co.uk>
2017-01-28 11:35:22 +0000

    Completes Grid unit tests

--- tests/Grid_tests.c
+++ tests/Grid_tests.c
@@ -41,7 +41,7 @@ MU_TEST(calls_new_sets_correct_empty_square)
     mu_check(grid->empty_square == empty_square);
 }
 
-MU_TEST(calls_get_sets_grid_correctly)
+MU_TEST(calls_set_sets_grid_correctly)
 {
     // arrange
     Grid *grid = Grid_new(4, 4);
@@ -53,6 +53,90 @@ MU_TEST(calls_get_sets_grid_correctly)
     mu_check(grid->grid[0][0] == 'T');
 }
 
+MU_TEST(calls_set_returns_true_when_in_range)
+{
+    // arrange
+    Grid *grid = Grid_new(4, 4);
+    int valid;
+
+    // act
+    valid = grid->set(grid, 0, 0, 'T');
+
+    // assert
+    mu_check(valid);
+}
+
+MU_TEST(calls_set_returns_false_when_out_of_range)
+{
+    // arrange
+    Grid *grid = Grid_new(4, 4);
+    int valid;
+
+    // act
+    valid = grid->set(grid, 4, 4, 'T');
+
+    // assert
+    mu_check(!valid);
+}
+
+MU_TEST(calls_is_in_range_returns_true_when_in_range)
+{
+    // arrange
+    Grid *grid = Grid_new(4, 4);
+    int valid;
+
+    // act
+    valid = grid->is_in_range(grid, 0, 0);
+
+    // assert
+    mu_check(valid);
+}
+
+MU_TEST(calls_is_in_range_returns_false_when_out_of_range)
+{
+    // arrange
+    Grid *grid = Grid_new(4, 4);
+    int valid;
+
+    // act
+    valid = grid->is_in_range(grid, 4, 4);
+
+    // assert
+    mu_check(!valid);
+}
+
+MU_TEST(calls_get_returns_set_char)
+{
+    // arrange
+    Grid *grid = Grid_new(4, 4);
+    char return_value;
+    char set_value = 'T';
+
+    grid->set(grid, 0, 0, set_value);
+
+    // act
+    return_value = grid->get(grid, 0, 0);
+
+    // assert
+    mu_check(return_value == set_value);
+}
+
+MU_TEST(calls_get_returns_empty_square_when_out_of_range)
+{
+    // arrange
+    Grid *grid = Grid_new(4, 4);
+    char return_value;
+    char set_value = 'T';
+
+    grid->set(grid, 4, 4, set_value);
+
+    // act
+    return_value = grid->get(grid, 4, 4);
+
+    // assert
+    mu_check(return_value == grid->empty_square);
+}
+
 void Grid_tests()
 {
     printf("Grid_tests");
@@ -60,7 +144,13 @@ void Grid_tests()
     MU_RUN_TEST(calls_new_default_sets_correct_columns);
     MU_RUN_TEST(calls_new_default_sets_correct_rows);
     MU_RUN_TEST(calls_new_sets_correct_empty_square);
-    MU_RUN_TEST(calls_get_sets_grid_correctly);
+    MU_RUN_TEST(calls_set_sets_grid_correctly);
+    MU_RUN_TEST(calls_set_returns_true_when_in_range);
+    MU_RUN_TEST(calls_set_returns_false_when_out_of_range);
+    MU_RUN_TEST(calls_is_in_range_returns_true_when_in_range);
+    MU_RUN_TEST(calls_is_in_range_returns_false_when_out_of_range);
+    MU_RUN_TEST(calls_get_returns_set_char);
+    MU_RUN_TEST(calls_get_returns_empty_square_when_out_of_range);
 
     MU_REPORT();
 }
