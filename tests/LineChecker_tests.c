d1d27a82b3ac1bd6e49974f415d3d25a2b611a59
logiclogue <jordanpaullord@hotmail.co.uk>
2017-01-29 13:34:28 +0000

    Adds tests for LineChecker_is_horizontal_line, found bug

--- tests/LineChecker_tests.c
+++ tests/LineChecker_tests.c
@@ -1,6 +1,7 @@
 #include "../deps/minunit/minunit.h"
 #include "LineChecker_tests.h"
 #include "../src/LineChecker.h"
+#include "../src/Board.h"
 
 static Grid *grid;
 
@@ -68,15 +69,42 @@ MU_TEST(calls_is_line_returns_false_when_none_return_true)
     mu_check(!return_value);
 }
 
+Grid *get_horizontal_line_grid(void)
+{
+    Board *board = Board_new(3, 3);
+    char piece = 'T';
+
+    board->input(board, 0, piece);
+    board->input(board, 1, piece);
+    board->input(board, 2, piece);
+
+    return (Grid *)board;
+}
+
+MU_TEST(calls_is_horizontal_line_when_is_line_then_return_true)
+{
+    // arrange
+    Grid *grid = get_horizontal_line_grid();
+    LineChecker *line_checker = LineChecker_new(grid, 3);
+    int return_value;
+
+    // act
+    return_value = line_checker->is_horizontal_line(line_checker);
+
+    // assert
+    mu_check(return_value);
+}
+
 void LineChecker_tests()
 {
     printf("LineChecker_tests");
 
-    grid = Grid_new(4, 4);
+    grid = Grid_new(3, 3);
 
     MU_RUN_TEST(calls_is_line_returns_true_when_all_return_true);
     MU_RUN_TEST(calls_is_line_returns_true_when_one_return_true);
     MU_RUN_TEST(calls_is_line_returns_false_when_none_return_true);
+    MU_RUN_TEST(calls_is_horizontal_line_when_is_line_then_return_true);
 
     MU_REPORT();
 }
