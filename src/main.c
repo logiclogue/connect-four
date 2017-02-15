f297b462563a18cfdb8eb2df8a371471c197503e
logiclogue <jordanpaullord@hotmail.co.uk>
2017-02-15 22:20:05 +0000

    Adds basic game demo in main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
-#include "Board.h"
+#include "Game.h"
 
 #ifdef BUILD_MAIN
 int main(void)
 {
-    Board *board = Board_new(7, 6);
+    Board *board = Board_new(3, 3);
+    LineChecker *line_checker = LineChecker_new((Grid *)board, 3);
+    Game *game = Game_new(NULL, NULL, board, line_checker);
+    int column;
 
-    int result = board->input(board, 3, 3);
+    while (!game->is_game_over(game)) {
+        scanf("%d", &column);
 
-    return 0;
+        game->input_move(game, game->player_to_move, column);
+
+        int x, y;
+
+        for (x = board->columns - 1; x >= 0; x -= 1) {
+            for (y = 0; y < board->rows; y += 1) {
+                printf("%p", board->get(board, y, x));
+            }
+            printf("\n");
+        }
+    }
 }
 #endif
