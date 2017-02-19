87dc303fb860ba6be96aeca2dd5c944adab7badc
logiclogue <jordanpaullord@hotmail.co.uk>
2017-02-19 13:40:42 +0000

    Fixes compiling errors with GridToString

--- src/GridToString.c
+++ src/GridToString.c
@@ -1,9 +1,10 @@
+#include <stdlib.h>
 #include "GridToString.h"
 
 GridToString *GridToString_new(
     Grid *grid,
     Player *player_1_ptr,
-    Player *player_2_ptr);
+    Player *player_2_ptr)
 {
     GridToString *self = CLASS_MALLOC(GridToString);
 
@@ -20,4 +21,29 @@ GridToString *GridToString_new(
 GridToString *GridToString_new_default(void)
 {
     return GridToString_new(NULL, NULL, NULL);
+}
+
+void GridToString_apply(GridToString *self)
+{
+    self->new = GridToString_new_default;
+    self->apply = GridToString_apply;
+    self->destroy = (void *)Class_destroy;
+    self->get = GridToString_get;
+    self->get_row = GridToString_get_row;
+    self->get_char = GridToString_get_char;
+}
+
+char *GridToString_get(GridToString *self)
+{
+    return "";
+}
+
+char *GridToString_get_row(GridToString *self, int column)
+{
+    return "";
+}
+
+char GridToString_get_char(GridToString *self, void *player_ptr)
+{
+    return self->empty_char;
 }
--- src/GridToString.h
+++ src/GridToString.h
@@ -7,20 +7,22 @@
 #include "Grid.h"
 
 #define GRID_TO_STRING_PROPS(self_t) \
+    Grid *grid; \
     void *player_1_ptr; \
     void *player_2_ptr; \
     char player_1_char; \
     char player_2_char; \
     char empty_char; \
     char *(*get)(self_t *self); \
-    char *(*get_row)(self_t *self);
+    char *(*get_row)(self_t *self, int column); \
+    char (*get_char)(self_t *self, void *player_ptr);
 
 typedef struct _GridToString GridToString;
 
 struct _GridToString {
     CLASS_PROPS(GridToString)
     GRID_TO_STRING_PROPS(GridToString)
-}
+};
 
 GridToString *GridToString_new(
     Grid *grid,
@@ -30,5 +32,6 @@ GridToString *GridToString_new_default(void);
 void GridToString_apply(GridToString *self);
 char *GridToString_get(GridToString *self);
 char *GridToString_get_row(GridToString *self, int row);
+char GridToString_get_char(GridToString *self, void *player_ptr);
 
 #endif
