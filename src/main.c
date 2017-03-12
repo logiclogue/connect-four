09a117dcf39bce735c44c7dc15b01b66b0850be4
logiclogue <jordanpaullord@hotmail.co.uk>
2017-03-12 10:42:25 +0000

    Adds command line argument for line length

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include "GridToString.h"
 #include "Grid.h"
 #include "Board.h"
+#include "LineChecker.h"
 
 #ifdef BUILD_MAIN
 static char *get_column_string(int columns)
@@ -29,7 +30,11 @@ int main(int argc, char *argv[])
     Board *board;
     Player *player_1, *player_2;
     GridToString *converter;
-    int columns, rows, move, i;
+    LineChecker *line_checker;
+    int columns = 7;
+    int rows = 6;
+    int line_length = 4;
+    int move, i;
     char *flag, *argument, *column_string;
 
     for (i = 0; i < argc; i += 1) {
@@ -44,13 +49,16 @@ int main(int argc, char *argv[])
             columns = atoi(argument);
         } else if (!strcmp(flag, "--rows")) {
             rows = atoi(argument);
+        } else if (!strcmp(flag, "--length")) {
+            line_length = atoi(argument);
         }
     }
 
     column_string = get_column_string(columns);
     board = Board_new(columns, rows);
+    line_checker = LineChecker_new((Grid *)board, line_length);
     printf("%d, %d\n", columns, rows);
-    game = Game_new(NULL, NULL, board, NULL);
+    game = Game_new(NULL, NULL, board, line_checker);
     player_1 = game->player_1;
     player_2 = game->player_2;
     converter = GridToString_new((Grid *)board, player_1, player_2);
