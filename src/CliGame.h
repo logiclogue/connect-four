221cf09b48712546d8f46ef20fd94b964cdecb0c
logiclogue <jordanpaullord@hotmail.co.uk>
2017-03-18 22:20:31 +0000

    Adds basic class method declarations into header file for CliGame

--- src/CliGame.h
+++ src/CliGame.h
@@ -11,4 +11,12 @@ struct _CliGame {
     GAME_MANAGER_INTERFACE_PROPS(CliGame)
 };
 
+CliGame_new(
+    Game *game,
+    Grid *grid,
+    LineChecker *line_checker);
+CliGame_new_default(void);
+CliGame_apply(CliGame *self);
+CliGame_destroy(CliGame *self);
+
 #endif
